fc2dc98283cb91a3a1613d613d41720fb41d78b7
Simon Gough <simon@oncebyten.com>
2017-11-14 16:35:14 +0000

    allow fadeout at interval

--- android/src/main/java/com/sh3rawi/RNAudioPlayer/RNAudioPlayerModule.java
+++ android/src/main/java/com/sh3rawi/RNAudioPlayer/RNAudioPlayerModule.java
@@ -12,9 +12,12 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import android.content.Context;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.SeekBar;
 
@@ -26,11 +29,14 @@ import java.util.TimerTask;
 public class RNAudioPlayerModule extends ReactContextBaseJavaModule implements Runnable {
   ReactApplicationContext reactContext;
   MediaPlayer mp;
+  private static AudioManager audioManager;
   private float mVolume = 1;
+  private int mFadeOutTime = 0;
 
   public RNAudioPlayerModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    audioManager = (AudioManager) reactContext.getSystemService(Context.AUDIO_SERVICE);
   }
 
   @Override
@@ -38,36 +44,94 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule implements R
     return "RNAudioPlayer";
   }
 
+  private static float getDeviceVolume() {
+        int volumeLevel = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        return (float) volumeLevel / maxVolume;
+  }
+
+  @ReactMethod
+  public void setFadeOutInterval(final int fadeOutTime) {
+      if(mp!=null) {
+          try {
+              //Log.d("AUDIO", "duration: " + String.valueOf(mp.getDuration()) + ", fadeOutTime: " + String.valueOf(fadeOutTime));
+
+              if (mp.getDuration() > fadeOutTime) {
+                  mFadeOutTime = fadeOutTime * 1000;
+              }
+          }
+          catch (Exception e){
+              Log.d("AUDIO", e.getMessage());
+          }
+      }
+  }
+
   @ReactMethod
   public void startFadeOut() {
+
       if(mp!=null) {
-          final Timer timer = new Timer(true);
-          TimerTask timerTask = new TimerTask()
-          {
+          final int duration = 2000;    //2sec
+          final float deviceVolume = getDeviceVolume();
+
+          //Log.d("AUDIO", "deviceVolume: " + String.valueOf(deviceVolume));
+
+          final Handler h = new Handler();
+          h.postDelayed(new Runnable() {
+              private float time = duration;
+              private float volume = 0.0f;
+
               @Override
-              public void run()
-              {
-                  //Log.i("RNAudio - mVolume: ", String.valueOf(mVolume));
-                  if(mVolume>0.1) {
-                      mVolume = mVolume-0.1f;
-                      mp.setVolume(mVolume,mVolume);
-                  }
-                  else {
-                      try {
-                          mp.setVolume(0, 0);
+              public void run() {
+                  try {
+                      if (!mp.isPlaying())
+                          mp.start();
+                      time -= 100;
+                      volume = (deviceVolume * time) / duration;
 
 
-                          mp.seekTo(mp.getDuration() - 10);
+                      //Log.d("AUDIO", "deviceVolume: " + String.valueOf(deviceVolume) + ", new vol: " + String.valueOf(volume));
+
+                      mp.setVolume(volume, volume);
+                      if (time > 0)
+                          h.postDelayed(this, 100);
+                      else {
+                          mp.seekTo(mp.getDuration());
+                          mp.stop();
+                          mp.release();
                       }
-                      catch(Exception e) {}
-                      timer.cancel();
-                      timer.purge();
-
                   }
+                  catch (Exception e) {}
               }
-          };
+          }, 100);
 
-          timer.schedule(timerTask,250,250);
+//          final Timer timer = new Timer(true);
+//          TimerTask timerTask = new TimerTask()
+//          {
+//              @Override
+//              public void run()
+//              {
+//                  //Log.i("RNAudio - mVolume: ", String.valueOf(mVolume));
+//                  if(mVolume>0.1) {
+//                      mVolume = mVolume-0.1f;
+//                      mp.setVolume(mVolume,mVolume);
+//                  }
+//                  else {
+//                      try {
+//                          mp.setVolume(0, 0);
+//
+//
+//                          mp.seekTo(mp.getDuration() - 10);
+//                      }
+//                      catch(Exception e) {}
+//                      timer.cancel();
+//                      timer.purge();
+//
+//                  }
+//              }
+//          };
+//
+//          timer.schedule(timerTask,250,250);
       }
   }
 
@@ -76,7 +140,7 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule implements R
     if(mp!=null) {
         Float newval = val*1000.0f;
 
-        Log.d("RNAudioPlayer", newval + " of " + mp.getDuration());
+        //Log.d("RNAudioPlayer", newval + " of " + mp.getDuration());
 
         if(newval>=0 && newval<=mp.getDuration()) {
             mp.seekTo(newval.intValue());
@@ -139,9 +203,9 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule implements R
     @ReactMethod
     public void playFromExpansion(String audio, int mainVersion, int patchVersion) {
         if(audio!=null) {
-            Log.d("AudioDEBUG", "audio: " + audio);
-            Log.d("AudioDEBUG", "mainVersion: " + String.valueOf(mainVersion));
-            Log.d("AudioDEBUG", "patchVersion: " + String.valueOf(patchVersion));
+//            Log.d("AudioDEBUG", "audio: " + audio);
+//            Log.d("AudioDEBUG", "mainVersion: " + String.valueOf(mainVersion));
+//            Log.d("AudioDEBUG", "patchVersion: " + String.valueOf(patchVersion));
             String fname = audio.toLowerCase().replace(".mp3","") + ".mp3";
 
             ZipResourceFile expansionFile= null;
@@ -149,7 +213,7 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule implements R
             try {
                 expansionFile = APKExpansionSupport.getAPKExpansionZipFile(this.reactContext, mainVersion, patchVersion);
                 if(expansionFile!=null) {
-                    Log.d("AudioDEBUG", "expansionFile NOT null: " + fname);
+                    //Log.d("AudioDEBUG", "expansionFile NOT null: " + fname);
                     fd = expansionFile.getAssetFileDescriptor(fname);
                 }
                 else {
@@ -277,6 +341,15 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule implements R
             } catch (Exception e) {
                 return;
             }
+
+            try {
+                //Log.d("AUDIO", "pos: " + String.valueOf(currentPosition) + ", fade: " + String.valueOf(mFadeOutTime));
+                if(currentPosition > mFadeOutTime && mFadeOutTime>0) {
+                    this.startFadeOut();
+                }
+            }
+            catch (Exception e) {}
+
             WritableMap map = Arguments.createMap();
 
             map.putDouble("currentTime", (currentPosition/1000.0f));
